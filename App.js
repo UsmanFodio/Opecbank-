9fb64478c3209b2b82946f110d9d0fcc0087ec66
UsmanFodio <OfficialUsmanDanfodio@gmail.com>
2025-05-14 02:12:39 +0100

    Add files via upload
    Signed-off-by: UsmanFodio <OfficialUsmanDanfodio@gmail.com>

---
+++ App.js
@@ -0,0 +1,8 @@
+// Paste the full Opecbank React app code here
+import React from 'react';
+
+const App = () => {
+  return <div>Opecbank App - Replace with full code</div>;
+};
+
+export default App;
