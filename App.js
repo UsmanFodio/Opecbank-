ea29947ddccaf89d0cfb5a16b70605483b2e5d96
UsmanFodio <OfficialUsmanDanfodio@gmail.com>
2025-05-14 02:32:08 +0100

    Delete App.js
    Signed-off-by: UsmanFodio <OfficialUsmanDanfodio@gmail.com>

--- App.js
+++
@@ -1,8 +0,0 @@
-// Paste the full Opecbank React app code here
-import React from 'react';
-
-const App = () => {
-  return <div>Opecbank App - Replace with full code</div>;
-};
-
-export default App;
